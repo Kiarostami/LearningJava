9cb01185f9b992b6e2d6859a7f0350b3492252cd
Sina Kiarostami <48089001+Kiarostami@users.noreply.github.com>
2022-01-01 23:29:57 +0200

    Update and rename Loops1.java to Multiplication Table.java

--- Multiplication Table.java
+++ Multiplication Table.java
@@ -8,7 +8,7 @@ import java.util.regex.*;
 
 
 
-public class Solution {
+public class Multiplication Table {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
